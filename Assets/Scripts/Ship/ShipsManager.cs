8a4823c65fd2ef8056df725d0eba16c85004f737
Eliott Guignabaudet <eliott.guignabaudet@gmail.com>
2023-04-29 22:15:40 +0200

    set ship manager as singleton

--- Assets/Scripts/Ship/ShipsManager.cs
+++ Assets/Scripts/Ship/ShipsManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class ShipsManager : MonoBehaviour
@@ -8,12 +9,28 @@ public class ShipsManager : MonoBehaviour
 
     public int MaxShip;
 
+    private static GameObject _instance;
+
+    public static GameObject Instance;
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Debug.Log("Destroy instance");
+            Destroy(gameObject);
+            return;
+        }
+        _instance = gameObject;
+    }
 
     void Start()
     {
         MaxShip = 1;
-        ships = new List<ShipStats>();
-        ships.Add(new ShipStats());
+        ships = new List<ShipStats>
+        {
+            new ShipStats()
+        };
         Debug.Log(ships[0].ToString());
     }
 
