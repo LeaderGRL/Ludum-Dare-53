ee0345fc51fa5938fb9e42d7154caefbd1258fb5
AlbaNagisa <alban.jeux2.0@gmail.com>
2023-04-30 19:24:22 +0200

    move ship

---
+++ Assets/Scripts/Ship/Move.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    void Update() {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
--- Assets/Scripts/Ship/Ship.cs
+++ Assets/Scripts/Ship/Ship.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,10 @@ using UnityEngine;
 public class Ship : MonoBehaviour
 {
     private ShipStats shipStats;
+    private bool collide;
+
+    private string planetName;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,22 @@ public class Ship : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        planetName = "Vespera";
+        if (planetName != null && !collide)
+        {
+            GetComponent<Move>().enabled = true;
+            GetComponent<Move>().target =
+                GameObject.Find(planetName).transform;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == planetName)
+        {
+            GetComponent<Move>().enabled = false;
+            collide = true;
+            // shipStats.AssignedQuest = null;
+        }
     }
 }
