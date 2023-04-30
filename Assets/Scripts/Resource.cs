3fb1c1fcb0402985f39714ef5bc7137cca13c5bd
Jojo <jordangrilly@gmail.com>
2023-04-30 14:54:24 +0200

    Merge

---
+++ Assets/Scripts/Resource.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Resource", menuName = "ScriptableObjects/Resource")]
+public class Resource : ScriptableObject
+{
+    public string name;
+    public int quantity;
+    public SpriteRenderer icon;
+    public int value;
+    public bool Stackable;
+}
