9b3db37ed1fa077e2fe762777661df9ef1314624
salexis <alexis.soubieux@ynov.com>
2023-05-01 14:00:10 +0200

    Affichage spaceship

--- Assets/Scripts/Inventory/AddInventoryItemDemo.cs
+++ Assets/Scripts/Inventory/AddInventoryItemDemo.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class AddInventoryItemDemo : MonoBehaviour
 {
-    // public Resource item;
-
-    // public Resource item2;
-
-    public Sprite imageSpaceShip;
     public Sprite imageNone;
     
     // Start is called before the first frame update
@@ -23,22 +18,5 @@ public class AddInventoryItemDemo : MonoBehaviour
 
             InventoryManager.instance.Add(item);
         }
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-    //     if (!Input.GetKeyDown(KeyCode.K)) return;
-    //     item.type = "SpaceShip";
-    //     item.currentLocation = "Testy";
-    //     item.fuelLevel = 0.5f;
-    //     item.icon = imageSpaceShip;
-    //     InventoryManager.instance.Add(item);
-    //         
-    //     item2.type = "SpaceShip";
-    //     item2.currentLocation = "Lyon";
-    //     item2.fuelLevel = 0;
-    //     item2.icon = imageSpaceShip;
-    //     InventoryManager.instance.Add(item2);
     }
 }
